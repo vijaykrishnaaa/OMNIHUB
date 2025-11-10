c18ba5582b70a7770f305f535498ef6a582d88d7
vijaykrishnaaa <144677323+vijaykrishnaaa@users.noreply.github.com>
2025-11-10 11:54:09 +0530

    Import API_URL in CollectionsModal component

--- src/modules/CollectionsModule/CollectionsModal.jsx
+++ src/modules/CollectionsModule/CollectionsModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { FolderKanban, X, Edit, Trash2, Check, XCircle } from 'lucide-react';
+import { API_URL } from '../../../apiConfig';
 
 const CollectionsModal = ({ onClose, onNavigate }) => {
     const { user } = useAuth();
